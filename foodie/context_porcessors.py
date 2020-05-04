54d2049437cc91e40afe4ded380ad4d14c468c44
daisuke524 <daisuke.u524@gmail.com>
2020-05-04 19:03:46 +0900

    ファイル移動

--- foodie/context_porcessors.py
+++
@@ -1,9 +0,0 @@
-from .models import Pref, Category
-
-
-def common(request):
-    context = {
-        'pref_list': Pref.objects.all().order_by('pref'),
-        'category_list': Category.objects.all().order_by('category_l'),
-    }
-    return context
