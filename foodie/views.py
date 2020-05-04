ecce658af9d1a1b4d65915345317a729630504a5
daisuke524 <daisuke.u524@gmail.com>
2020-05-04 18:27:52 +0900

    改行整理

--- foodie/views.py
+++ foodie/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-# Create your views here.
-
 GNAVI_URL = "https://api.gnavi.co.jp/RestSearchAPI/v3/"
 GNAVI_KEY = "xxxxx"
-
 
 class IndexView(TemplateView):
     template_name = 'foodie/index.html'
